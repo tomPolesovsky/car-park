f94a80bcf72839f31482de4067c386fd8c349e9b
svoren258 <svoren258@gmail.com>
2018-11-26 22:38:28 +0100

    ReservationSettingsFacade - commented. VehicleFacade - commented. EmployeeFacadeImpl - commented. VehicleFacadeImpl - commented. EmployeeService - commented. ReservationSettingsService - commented. VehicleService - commented.

--- carpark-service/src/main/java/cz/pa165/carpark/service/EmployeeService.java
+++ carpark-service/src/main/java/cz/pa165/carpark/service/EmployeeService.java
@@ -9,16 +9,48 @@ import java.util.List;
  */
 public interface EmployeeService {
 
+    /**
+     * Find employee for the specified username
+     *
+     * @param username
+     * @return employee
+     */
     Employee findByUsername(String username);
 
+    /**
+     * Find the employee with the specified id
+     *
+     * @param id unambiguous identification of entity
+     * @return employee
+     */
     Employee find(Long id);
 
+    /**
+     * Find all employees
+     *
+     * @return list of all employees
+     */
     List<Employee> findAll();
 
+    /**
+     * Save the employee
+     *
+     * @param employee
+     */
     void save(Employee employee);
 
+    /**
+     * Update the specified employee
+     *
+     * @param employee
+     */
     void update(Employee employee);
 
+    /**
+     * Delete the employee with the specified id
+     *
+     * @param id unambiguous identification of entity
+     */
     void delete(Long id);
 
 }
