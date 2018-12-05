51b5da556f8ef5a44b04efbd6a66b2030b125f54
Tomáš Polešovský <tomas.polesovsky@irvekon.cz>
2018-12-05 22:41:21 +0100

    Added rest module

---
+++ carpark-rest/src/main/java/cz/pa165/carpark/util/ApiUtils.java
@@ -0,0 +1,13 @@
+package cz.pa165.carpark.util;
+
+import java.util.function.Supplier;
+
+public class ApiUtils {
+
+    public static <T extends RuntimeException> void notNull(Object o, Supplier<T> supplier) {
+        if (o == null) {
+            throw supplier.get();
+        }
+    }
+
+}
