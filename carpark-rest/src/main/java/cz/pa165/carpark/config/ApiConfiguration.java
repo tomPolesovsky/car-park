50f0cd75e8d856b8ee983e65ef1d48aeecad49ce
svoren258 <svoren258@gmail.com>
2018-12-06 22:08:37 +0100

    ReservationSettingsController added. ApiConfiguration - api paths added.

--- carpark-rest/src/main/java/cz/pa165/carpark/config/ApiConfiguration.java
+++ carpark-rest/src/main/java/cz/pa165/carpark/config/ApiConfiguration.java
@@ -4,5 +4,7 @@ public abstract class ApiConfiguration {
 
     public static final String API_EMPLOYEES   = "/employees";
     public static final String API_LOGIN  = "/login";
-
+    public static final String API_VEHICLES   = "/vehicles";
+    public static final String API_RESERVATIONS   = "/reservations";
+    public static final String API_RESERVATION_SETTINGS   = "/reservation-settings";
 }
