c4e32ec1e4cfa1bb1e0affe4a122277041fe5673
svoren258 <svoren258@gmail.com>
2018-12-17 20:50:47 +0100

    Alerts added - missing reservation settings or in case allowed is set to false.

--- carpark-web/src/main/webapp-src/src/app/shared/services/reservation-settings.service.ts
+++ carpark-web/src/main/webapp-src/src/app/shared/services/reservation-settings.service.ts
@@ -3,6 +3,7 @@ import {Injectable} from "@angular/core";
 import {ReservationSettings} from "../models/reservation-settings.model";
 import {requestPath} from "../utils";
 import {Reservation} from "../models/reservation.model";
+import {Employee} from "../models/employee.model";
 
 const httpOptions = {
   headers: new HttpHeaders(({'Content-Type': 'application/json'}))
@@ -12,6 +13,7 @@ const httpOptions = {
 export class ReservationSettingsService {
 
   private reservationSettingsUrl = '/reservation-settings';
+  private findbyEmployeeUrl = '/find-by-employee';
 
   constructor(private readonly http: HttpClient) {
   }
@@ -30,5 +32,9 @@ export class ReservationSettingsService {
 
   updateReservationSettings(reservationSettings: ReservationSettings) {
     return this.http.put<Reservation>(`${requestPath}${this.reservationSettingsUrl}`, reservationSettings);
+  }
+
+  getReservationSettingsByEmployee(employee: Employee) {
+    return this.http.post<ReservationSettings>(`${requestPath}${this.reservationSettingsUrl}${this.findbyEmployeeUrl}`, employee);
   }
 }
